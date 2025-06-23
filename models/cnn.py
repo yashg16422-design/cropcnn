00559a4bf4a66936f928b6ba139b73f34475b92e
Yash Pratap Singh <yashg16422@gmail.com>
2025-06-23 16:33:00 +0530

    Refactoring and updating cnn.py

---
+++ models/cnn.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        self.classifier = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        return self.classifier(x)
